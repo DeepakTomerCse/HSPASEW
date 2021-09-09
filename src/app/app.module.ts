4446adfb6e4d1d4d3b8a90bce905a4064b8c6173
Noida Office <us_noida@hotmail.com>
2021-09-09 21:03:27 +0530

    master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { F1Component } from './Footer/f1/f1.component';
-import { C003Component } from './Body/c003/c003.component';
-import { C002Component } from './Body/c002/c002.component';
-import { C001Component } from './Body/c001/c001.component';
-import { H1Component } from './header/h1/h1.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    H1Component,
-    F1Component,
-    C001Component,
-    C002Component,
-    C003Component
+    AppComponent
   ],
   imports: [
     BrowserModule,
