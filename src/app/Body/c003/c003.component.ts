54d1ff289f8935379c7072a6b2d7b25dec5ac6e3
Noida Office <us_noida@hotmail.com>
2021-09-09 19:02:01 +0530

    C001 Body Component

---
+++ src/app/Body/c003/c003.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-c003',
+  templateUrl: './c003.component.html',
+  styleUrls: ['./c003.component.css']
+})
+export class C003Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
